a39094f9b37af32fea8482b6b1a4c0d375860db6
Diffa <you@example.com~>
2025-04-17 14:23:12 +0700

    Diffa

--- controller/productController.js
+++ controller/productController.js
@@ -1,14 +1,9 @@
-const { product, cart, user, order } = require('../data/data');
+const { product, cart, user, order } = require("../data/data");
 
 module.exports.getAllProduct = (req, res) => {
+  console.log(req.query, "===> ini query");
+  res.status(200).json(product);
+};
+module.exports.getDetailProduct = (req, res) => {};
 
-}
-
-module.exports.getDetailProduct = (req, res) => {
-
-}
-
-module.exports.searchProduct = (req, res) => {
-
-}
-
+module.exports.searchProduct = (req, res) => {};
